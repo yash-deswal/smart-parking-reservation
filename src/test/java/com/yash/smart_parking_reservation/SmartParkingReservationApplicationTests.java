7e51c91bfe9d74032a0b568f1de6fd71a6934ff8
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-30 04:05:52 +0530

    Simplify test for Jenkins pipeline

--- src/test/java/com/yash/smart_parking_reservation/SmartParkingReservationApplicationTests.java
+++ src/test/java/com/yash/smart_parking_reservation/SmartParkingReservationApplicationTests.java
@@ -1,13 +1,10 @@
 package com.yash.smart_parking_reservation;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SmartParkingReservationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
